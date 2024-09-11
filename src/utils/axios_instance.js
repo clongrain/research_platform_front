a7bac4b8cc6d53b56a3676c9570ceec1239ae81d
CLrain <2394617038@qq.com>
2024-09-11 15:51:57 +0800

    fix REACT_APP_

--- src/utils/axios_instance.js
+++ src/utils/axios_instance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import storgeUtils from './storageUtils'
 export const baseURL = window.location.hostname.startsWith('192.168.')
-  ? process.env.BACKEND_URL_INTERNAL
-  : process.env.BACKEND_URL_VPN;
+  ? process.env.REACT_APP_BACKEND_URL_INTERNAL
+  : process.env.REACT_APP_BACKEND_URL_VPN;
 const axios_instance = axios.create({
   baseURL: baseURL,
   timeout: 10000000,
